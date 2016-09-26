e82bf2309eb6e1424e0e7290de3c177eed683254
drunk <danielchowofficial>
2016-09-25 17:37:54 -0700

    Blank methods created that are accessed from button clicks. Need to implement grid and movement and different pictures for different scenarios. All buttons will change the image now.

--- app/src/main/java/com/example/drunk/cornmazesimulator/ButtonActions.java
+++ app/src/main/java/com/example/drunk/cornmazesimulator/ButtonActions.java
@@ -9,31 +9,67 @@ import android.widget.ImageView;
 public class ButtonActions extends MainActivity implements View.OnClickListener {
         private ImageView img;
         private Button fowardButton;
+        private Button leftButton;
+        private Button rightButton;
+        private Button turnAroundButton;
         protected void onCreate(Bundle savedInstanceState) {
 
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.actual_game);
                 fowardButton = (Button) findViewById(R.id.FowardButton);
                 fowardButton.setOnClickListener(this);
+                leftButton = (Button) findViewById(R.id.LeftButton);
+                leftButton.setOnClickListener(this);
+                rightButton = (Button) findViewById(R.id.RightButton);
+                rightButton.setOnClickListener(this);
+                turnAroundButton = (Button) findViewById(R.id.TurnAroundButton);
+                turnAroundButton.setOnClickListener(this);
                 img = (ImageView) findViewById(R.id.corn);
         }
 
 
 
-        int counter = 0;
+        int counter = 1;
         public void onClick(View v){
-
+                counter = imageChange(counter);
                 if (v.getId() == R.id.FowardButton) {
-                        if (counter == 1) {
-                                img.setImageResource(R.drawable.closedlegs);
-                                counter = 0;
-                        } else {
-                                img.setImageResource(R.drawable.openlegs);
-                                counter = 1;
-                        }
+                       fowardMove();
+                }
+                if (v.getId() == R.id.RightButton) {
+                        rightMove();
+                }
+                if (v.getId() == R.id.LeftButton) {
+                        leftMove();
+                }
+                if (v.getId() == R.id.TurnAroundButton) {
+                        turnAroundMove();
                 }
 
 
+        }
+        public int imageChange(int counter){
+                if (counter == 1) {
+                        img.setImageResource(R.drawable.closedlegs);
+                        counter = 0;
+                } else {
+                        img.setImageResource(R.drawable.openlegs);
+                        counter = 1;
+                }
+
+              return counter;
+        }
+
+        public void fowardMove(){
+                //Implement movement on grid and change in graphic.
+        }
+        public void leftMove(){
+                //Implement movement on grid and change in graphic.
+        }
+        public void rightMove(){
+                //Implement movement on grid and change in graphic.
+        }
+        public void turnAroundMove(){
+                //Implement movement on grid and change in graphic.
         }
 
 }
